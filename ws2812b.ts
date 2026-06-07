a828b49fb38c2676f2cfa255451a61825f86330b
martinwork <martinwork@users.noreply.github.com>
2026-06-07 20:16:01 +0100

    Update ws2812b.ts

--- ws2812b.ts
+++ ws2812b.ts
@@ -1,11 +1,12 @@
 namespace ws2812b {
     //% shim=ws2812b::send_buffer
     export function sendBuffer(buf: Buffer, pin: DigitalPin) {
+        basic.pause(0)
     }
 
     //% shim=ws2812b::set_buffer_mode
     export function setBufferMode(pin: DigitalPin, mode: number) {
-
+        basic.pause(0)
     }
 
     export const BUFFER_MODE_RGB = 1
